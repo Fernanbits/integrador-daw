e91a1bc3d98d7a38a1c8a2239d01a8558917bc0b
María Jimena Fernández <mjime.fer77@gmail.com>
2026-06-11 14:00:26 -0300

    feat: refactorización visual del login a diseño fluido

--- frontend/src/app/auth/login/login-api-client.ts
+++ frontend/src/app/auth/login/login-api-client.ts
@@ -1,17 +1,59 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { LoginApiClient } from './login-api-client';
+import { MessageService } from 'primeng/api';
+import { AuthStore } from '../auth-store';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
-@Injectable({ providedIn: 'root' })
-export class LoginApiClient {
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  imports: [ButtonModule, InputTextModule, PasswordModule, ReactiveFormsModule],
+})
+export class Login {
+  private readonly loginApiClient: LoginApiClient = inject(LoginApiClient);
+  private readonly messageService: MessageService = inject(MessageService);
+  private readonly authStore: AuthStore = inject(AuthStore);
+  private readonly router: Router = inject(Router);
 
-    private readonly client: HttpClient = inject(HttpClient)
+  readonly form: FormGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    clave: new FormControl('', [Validators.required]),
+  });
 
-    iniciarSesion(nombre: string, clave: string): Observable<{ accessToken: string }> {
-
-        return this.client.post<{ accessToken: string }>("/api/v1/auth", { nombre, clave });
-
+  iniciarSesion() {
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Atención',
+        detail: 'Por favor, completa tu usuario y clave.',
+      });
+      return;
     }
 
+    const nombre: string = this.form.value.nombre!;
+    const clave: string = this.form.value.clave!;
+
+    this.loginApiClient.iniciarSesion(nombre, clave).subscribe({
+      next: (data) => {
+        this.authStore.guardarToken(data.accessToken);
+        this.router.navigateByUrl('/proyectos');
+      },
+      error: (err) => {
+        // Captura el mensaje específico del backend si existe
+        const errorMessage =
+          err.error?.message || 'Ha ocurrido un error al iniciar sesión en el servidor.';
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Acceso Denegado',
+          detail: errorMessage,
+        });
+      },
+    });
+  }
 }
